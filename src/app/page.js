cf6ac412aa2d8df2025d9e694a7ccf23617ed45a
CheatSetha <cheatsetha2001@gmail.com>
2023-05-19 15:26:21 +0700

    fallback ily

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export async function getCategory() {
 }
 
 export async function getuser() {
-	const res = await fetch("https://api.escuelajs.co/api/v1/users")
+	const res = await fetch("https://api.escuelajs.co/api/v1/users?limit=8")
 	const data = await res.json()
 	return data
 }
